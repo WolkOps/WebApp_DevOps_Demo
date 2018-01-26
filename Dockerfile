4e6a16e3e50d7a6928ca0e1d230d0c5e1989a630
itomaldonado <mo.maldonado@gmail.com>
2018-01-26 02:03:23 -0500

    deployment files

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,22 @@
-# OVS
+# SOS
 #
 # Version       latest
 FROM library/python:2.7.9
+LABEL maintainer "mo.maldonado@gmail.com"
 
 # Adding all files.
 ADD requirements.txt /home/python/
 ADD app.py /home/python/
 ADD templates/ /home/python/templates/
+ADD static/ /home/python/static/
 
 # Installing all requirements
+RUN pip install -q --upgrade pip
 RUN pip install -q -r /home/python/requirements.txt
+RUN mkdir -p /var/lib/sqlite
+
+# Expose Volume
+VOLUME ['/var/lib/sqlite']
 
 # Exposing port 80
 EXPOSE 80
---
+++ jenkinsfile.groovy
@@ -0,0 +1,131 @@
+#!groovy
+
+pipeline {
+   agent {
+      node {
+         label 'master'
+      }
+   }
+
+   parameters {
+      string(name: 'ghprbActualCommit', defaultValue: 'master', description: 'When starting build give the sha1 parameter commit id you want to build or refname (eg: origin/pr/9/head).')
+   }
+
+   environment {
+      TEST_RESULT_LOCATION    = "**/*-nosetests.xml"
+      SSH_CONFIG_BASE         = "/var/lib/jenkins/.ssh/config.d/${env.BUILD_TAG}"
+      DOCKER_IMAGE_NAME       = "demo-webapp"
+      DOCKER_IMAGE_VERSION    = "latest"
+      PYTHONPATH              ="${WORKSPACE}:${PYTHONPATH}"
+   }
+
+   stages {
+
+      stage('Unit Test') { // for display purposes
+         steps {
+
+            // Checkout code
+            checkout([
+               $class: 'GitSCM', 
+               userRemoteConfigs: [[
+                     url: 'https://github.com/WolkOps/WebApp_DevOps_Demo.git', 
+                     name: 'origin'
+               ]],
+               branches: [[name: "${params.ghprbActualCommit}"]],
+               extensions: [
+                  [$class: 'WipeWorkspace'] // wipe workspace before clone
+               ]
+             ])
+
+            sh '''
+            #!/bin/bash
+            set -e
+
+            # Create and activate virtual environment
+            virtualenv ./venv
+            set +x; . "./venv/bin/activate"; set -x;
+
+            # Install requirements
+            pip install -q -r requirements.txt
+
+            # Run unit tests
+            nosetests --nologcapture --nocapture --verbose --with-xunit --xunit-file="./unit-nosetests.xml" --where="./test";
+            '''
+         }         
+      }
+
+      stage('Build Docker Image') {
+         steps {
+            script{
+               docker.build('${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}')
+            }
+         }
+      }
+
+      stage('Push Docker Image') {
+         steps {
+            script {
+               docker.withRegistry('https://956975823273.dkr.ecr.us-east-2.amazonaws.com', 'ecr:us-east-2:demo-ecr-credentials') {
+                  docker.image('${DOCKER_IMAGE_NAME}').push('${DOCKER_IMAGE_VERSION}')
+               }
+            }
+         }
+      }
+
+      stage('Deploy to Test') {
+         steps {
+            sh '''
+            #!/bin/bash
+            set -e
+
+            # Run Deployment to Test
+            ktmpl ./deploy/sos-deployment-tmpl.yaml \
+               --parameter NAMESPACE test \
+               --parameter VERSION latest \
+               | kubectl apply -f -
+            '''
+         }
+      }
+
+      stage('Acceptance') {
+         steps {
+            sh '''
+            #!/bin/bash
+            set -e
+
+            # Run robot framework
+            '''
+         }
+      }
+
+      stage('Deploy to Prod') {
+         steps {
+            sh '''
+            #!/bin/bash
+            set -e
+
+            # Run Deployment to Prod
+            tmpl ./deploy/sos-deployment-tmpl.yaml \
+               --parameter NAMESPACE production \
+               --parameter VERSION latest \
+               | kubectl apply -f -
+            '''
+         }
+      }
+   }
+
+   post{
+      always {
+         // Print that pipeline is finished
+         echo 'Pipeline done, recording results and cleaning up environment...'
+
+         // Test Results
+         junit "${env.TEST_RESULT_LOCATION}"
+
+         // deactivate and destroy VENV
+         sh '''
+         rm -fr ./venv/
+         '''
+      }
+   }
+}
