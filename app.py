ecfd47f82c0320c8488601825e6dc17a4905c0e4
Rich <rich@wolkops.com>
2018-02-01 15:15:07 +0530

    demo RAE

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ debug = True
 
 # In-memory Databases - Dynamic Dictionaries
 orders = dict()
-products = ['Piano', 'Saxophone', 'Flute', 'Base', 'Drums', 'Viola', 'Violin']
+products = ['Guiatar', 'Piano', 'Saxophone', 'Flute', 'Base', 'Drums', 'Viola', 'Violin']
 bad_states = ['CA','TX','FL']
 
 # Local Storage DB
