03150b5d3fa7278b9f2f51b78771653f9395b256
Rich <rich@wolkops.com>
2018-02-01 15:45:37 +0530

    reset demo RAE

--- tests/test_app.py
+++ tests/test_app.py
@@ -61,14 +61,6 @@ class APITest(unittest.TestCase):
         # Check Results
         assert 200 == result.status_code
 
-        # Testing productType: Guitar
-        new_order = self.template_order.copy()
-        new_order['productType'] = 'Guitar'
-        order = json.dumps(new_order)
-        result = self.applications.post('/sos/orders', content_type='application/json', data=order)
-        json_result = json.loads(result.data)
-        # Check Results
-        assert 200 == result.status_code
 
     def test_order_creation(self):
         # Create order
